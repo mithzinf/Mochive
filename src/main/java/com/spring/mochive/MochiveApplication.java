d432eb0249a2929fcdd107d2c8b87cd32d1d041f
수진 <suzlnl@naver.com>
2023-06-17 14:43:08 +0900

    first commit

---
+++ src/main/java/com/spring/mochive/MochiveApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mochive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/mochive/MochiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mochive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
